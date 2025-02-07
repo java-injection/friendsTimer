0cd9f763ce845a0aff601b074f3d9b7d3af5ecee
DaviDio <david.cascella.5@gmail.com>
2025-02-08 00:39:35 +0100

    First Commit

---
+++ src/main/java/it/cascella/friendstimer/FriendsTimerApplication.java
@@ -0,0 +1,13 @@
+package it.cascella.friendstimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsTimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsTimerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/cascella/friendstimer/FriendsTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cascella.friendstimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsTimerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
